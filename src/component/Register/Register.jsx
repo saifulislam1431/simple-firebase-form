9f800f1645c49fbd886154eeccc75c5c44ada491
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-14 17:30:48 +0600

    Firebase auth with register from

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
 const Register = () => {
+    const auth = getAuth(app);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        // console.log(email, password);
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(res=>{
+            const loggedUser = res.user;
+            console.log(loggedUser);
+        })
+        .catch(error=>{
+            console.log("Error:" , error.message);
+        })
+
+    }
     return (
-        <div>
-            
+        <div className='px-10 py-3 flex items-center justify-center min-h-[calc(100vh-100px)]'>
+            <div>
+                <h1 className='font-extrabold text-2xl text-center py-3 text-purple-600'>Register Form</h1>
+                <form className='form' onSubmit={handleSubmit}>
+                    <input type="text" id="username" name="username" required placeholder='Your Username'/><br />
+                    <input type="email" id="email" name="email" required placeholder='Your Email' /><br />
+
+                    <input type="password" id="password" name="password" required placeholder='Password' /><br />
+
+                    <input type="submit" value="Register" className='btn-submit' />
+                </form>
+            </div>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9VxY7DDIy8HeEvTy7-rpudJ5QVzsDK4g",
+  authDomain: "email-pass-auth-83a5b.firebaseapp.com",
+  projectId: "email-pass-auth-83a5b",
+  storageBucket: "email-pass-auth-83a5b.appspot.com",
+  messagingSenderId: "991219357070",
+  appId: "1:991219357070:web:fb1c85545064946d3a40df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
